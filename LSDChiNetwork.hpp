306e87d7d57f284f12482fa52e020df0d1b4f58b
Fiona Clubb <fclubb@users.noreply.github.com>
2013-09-03 13:23:50 +0000

    Added channel head prediction function to header file

--- LSDChiNetwork.hpp
+++ LSDChiNetwork.hpp
@@ -655,6 +655,15 @@ class LSDChiNetwork
 		/// @param minimum_segment_length How many nodes the mimimum segment will have.
 		/// @param N
 		void is_channel_long_enough_test(int minimum_segment_length,int N);
+		
+		/// @brief this function fits 2 segments to the chi-elevation data from first order basins and calculates
+		/// the most likely position of the channel head - added by FC 28/06/13
+		///
+		/// @param minimum_seg_length_for_channel_heads Minimum number of nodes used for segment fitting
+		/// @return array with channel head locations 
+		/// @author Fiona Clubb
+		/// @date 03/09/2013
+		Array2D<double> calculate_channel_heads(int min_seg_length_for_channel_heads);
 
 	protected:
 	
