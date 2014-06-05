f8ed3a5588522609de2d360396c0168b345b567b
s1143956 <DTMilodowski@users.noreply.github.com>
2014-06-05 10:57:13 +0000

    updated driver function for pelletier channel head extraction.  Note that this take quite a long time as it needs to do some long wavelenght surface fitting.  I would suggest running it once, and then loading in the two curvature rasters rather than recalculating each time

--- driver_functions/pelletier_driver_DTM.cpp
+++ driver_functions/pelletier_driver_DTM.cpp
@@ -57,15 +57,14 @@
 #include <iomanip>
 #include <math.h>
 #include <string.h>
-#include "../LSDStatsTools.hpp"
-#include "../LSDRaster.hpp"
-#include "../LSDRasterSpectral.hpp"
-#include "../LSDIndexRaster.hpp"
-#include "../LSDFlowInfo.hpp"
-#include "../LSDJunctionNetwork.hpp"
-#include "../LSDIndexChannelTree.hpp"
-#include "../LSDChiNetwork.hpp"
-using namespace std;
+#include "../../LSDStatsTools.hpp"
+#include "../../LSDRaster.hpp"
+#include "../../LSDRasterSpectral.hpp"
+#include "../../LSDIndexRaster.hpp"
+#include "../../LSDFlowInfo.hpp"
+#include "../../LSDJunctionNetwork.hpp"
+#include "../../LSDIndexChannelTree.hpp"
+#include "../../LSDChiNetwork.hpp"
 
 int main (int nNumberofArgs,char *argv[])
 {
@@ -117,8 +116,8 @@ int main (int nNumberofArgs,char *argv[])
 //   float FHigh = 0.1;
 //   LSDRaster topo_test_filtered = SpectralRaster.fftw2D_filter(FilterType, FLow, FHigh);
   LSDRaster topo_test_wiener = SpectralRaster.fftw2D_wiener();
-  int border_width = 50;	
-  LSDRaster topo_test_filtered = topo_test_wiener.border_with_nodata(border_width);
+  int border_width = 100;	
+//   topo_test_filtered = topo_test_filtered.border_with_nodata(border_width);
 	topo_test_wiener = topo_test_wiener.border_with_nodata(border_width);       
 	
 	// Set the no flux boundary conditions
@@ -130,7 +129,8 @@ int main (int nNumberofArgs,char *argv[])
 	
 		// get the filled file
 	cout << "Filling the DEM" << endl;
-	LSDRaster filled_topo_test = topo_test_filtered.fill(Minimum_Slope);
+// 	LSDRaster filled_topo_test = topo_test_filtered.fill(Minimum_Slope);
+	LSDRaster filled_topo_test = topo_test_wiener.fill(Minimum_Slope);
 	filled_topo_test.write_raster((DEM_f_name),DEM_flt_extension);
   
   //get a FlowInfo object
@@ -145,29 +145,33 @@ int main (int nNumberofArgs,char *argv[])
 	// Get the valleys using the contour curvature
 	
   int surface_fitting_window_radius = 7;
-  vector<LSDRaster> surface_fitting;
+  int surface_fitting_window_radius_LW = 25;
+  vector<LSDRaster> surface_fitting, surface_fitting_LW;
   LSDRaster tan_curvature;
+  LSDRaster tan_curvature_LW;
   string curv_name = "_tan_curv";
   vector<int> raster_selection(8, 0);
   raster_selection[6] = 1;
   surface_fitting = topo_test_wiener.calculate_polyfit_surface_metrics(surface_fitting_window_radius, raster_selection);
+  surface_fitting_LW = topo_test_wiener.calculate_polyfit_surface_metrics(surface_fitting_window_radius_LW, raster_selection);
   
-  for(int i = 0; i<int(raster_selection.size()); ++i)
+  for(int i = 0; i<raster_selection.size(); ++i)
 	{
 		if(raster_selection[i]==1)
 		{
       tan_curvature = surface_fitting[i];
       tan_curvature.write_raster((path_name+DEM_name+curv_name), DEM_flt_extension);
+      tan_curvature_LW = surface_fitting[i];
+      tan_curvature_LW.write_raster((path_name+DEM_name+curv_name+"_LW"), DEM_flt_extension);
     }
   }
 
-	string CH_name = "_CH_pelletier";
-	string complete_fname = path_name+DEM_name+CH_name+".flt";
-	
+	string CH_name = "_CH";
 	Array2D<float> topography = filled_topo_test.get_RasterData();
 	Array2D<float> curvature = tan_curvature.get_RasterData();
+	Array2D<float> curvature_LW = tan_curvature_LW.get_RasterData();
 	cout << "\tLocating channel heads..." << endl;
-  vector<int> ChannelHeadNodes = ChanNetwork.calculate_pelletier_channel_heads_DTM(FlowInfo, topography, curv_threshold, curvature);
+  vector<int> ChannelHeadNodes = ChanNetwork.calculate_pelletier_channel_heads_DTM(FlowInfo, topography, curv_threshold, curvature,curvature_LW);
   
   // Now filter out false positives along channel according to a threshold 
   // catchment area
@@ -187,11 +191,11 @@ int main (int nNumberofArgs,char *argv[])
   LSDIndexRaster Channel_heads_raster = FlowInfo.write_NodeIndexVector_to_LSDIndexRaster(ChannelHeadNodesFilt);
   Channel_heads_raster.write_raster((path_name+DEM_name+CH_name),DEM_flt_extension);
 	
-// 	//create a channel network based on these channel heads
-// 	LSDJunctionNetwork NewChanNetwork(ChannelHeadNodes, FlowInfo);
-//   LSDIndexRaster SOArrayNew = NewChanNetwork.StreamOrderArray_to_LSDIndexRaster();
-// 	string SO_name_new = "_SO_from_CH";
-// 	
-// 	SOArrayNew.write_raster((path_name+DEM_name+SO_name_new),DEM_flt_extension);	
+	//create a channel network based on these channel heads
+	LSDJunctionNetwork NewChanNetwork(ChannelHeadNodesFilt, FlowInfo);
+  LSDIndexRaster SOArrayNew = NewChanNetwork.StreamOrderArray_to_LSDIndexRaster();
+	string SO_name_new = "_SO_from_CH";
+	
+	SOArrayNew.write_raster((path_name+DEM_name+SO_name_new),DEM_flt_extension);	
                               
 }
