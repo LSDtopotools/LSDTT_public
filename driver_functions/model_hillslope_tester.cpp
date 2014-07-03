04df80a1b87697e25eca464d797973fff7b92998
Simon Marius Mudd <simon-m-mudd@users.noreply.github.com>
2014-07-03 12:23:57 +0000

    Added a driver and makefile to test the nonlinear hillslope solver

--- driver_functions/model_hillslope_tester.cpp
+++ driver_functions/model_hillslope_tester.cpp
@@ -98,21 +98,83 @@ int main(int argc, char *argv[])
 
 	mod.print_parameters();
 
-  // run the model to steady state. 
-  // first change the default dimensions
-  int newrows = 300;
-  int newcols = 150;
-  mod.resize_and_reset(newrows,newcols);
+  // first change the size of the domain
+  int newrows = 30;
+  int newcols = 10;
+  float new_resolution = 2.5;
+  mod.resize_and_reset(newrows,newcols, new_resolution);
 
-  // and turn the hillslopes off, just to save some time
-  mod.set_hillslope(false);
+  // and turn the fluvial off, just to save some time
+  mod.set_fluvial(false);
   
-  // now run to steady state
-	mod.reach_steady_state();
+  // give an uplift rate for the surface
+  float uplift_rate = 0.0005;     // this is in mm/yr
+  
+  float max_elev = 2.0;
+  float edge_offset = 0.0;
+  
+
+  
+  // get a steady state hillslope
+  mod.initialise_nonlinear_SS(uplift_rate);
+  
+  // print this surface. This is for checking the steady state solution
+  string DEM_name = "hillslope_test_SS";
+  DEM_name = pathname+DEM_name;
+  string asc_ext = "asc";
+  mod.write_raster(DEM_name,asc_ext);
+  
+  // Now change to parabola
+  mod.initialise_parabolic_surface(max_elev, edge_offset);
+
+  // print this surface
+  DEM_name = "hillslope_test_parabola";
+  DEM_name = pathname+DEM_name;
+  mod.write_raster(DEM_name,asc_ext);  
+  
+	// make an uplift field for the tester
+	int uplift_mode = 0;
+	float timestep = 50.0;
+	mod.set_timeStep(timestep);
+	
+	// need to divide upflift rate by ts because this member function returns absoulte
+	// elevation gain in a timestep rather than the uplift rate
+	Array2D<float> uplift_field = mod.generate_uplift_field( uplift_mode, uplift_rate/timestep );
+	
+	cout << "Uplift field is: " <<  uplift_field << endl;
+	
+	int nrows = uplift_field.dim1();
+	int ncols = uplift_field.dim2();
+	Array2D<float> zero_array(nrows,ncols,0.0);
+	
+	// now run a few timesteps.
+	float t_ime = 0;
+	float iteration_tolerance = 0.000001;
+	
+	// initiate the assembler matrix
+	mod.MuddPILE_initiate_assembler_matrix();
+	
+	cout << "entering nl loop" << endl;
+	for(int ts = 1; ts<2001; ts++)
+	{
+	  
+    mod.MuddPILE_nonlinear_creep_timestep(uplift_field, zero_array, iteration_tolerance);
+    t_ime += timestep;
+    if (ts%50 == 0)
+    {
+      cout << "time is: " << t_ime << endl;
+    }
+  }
+  
+  // print the file at the end
+  string DEM_name2 = "hillslope_test_end";
+  DEM_name2 = pathname+DEM_name2;
+  mod.write_raster(DEM_name2,asc_ext);  
+	
 	
 	//mod.run_model_from_steady_state();
 	//mod.run_model();
-	cout << "\a";
+	//cout << "\a";
 	//mod.slope_area();
 	//mod.show();
 	//ss.str("");
