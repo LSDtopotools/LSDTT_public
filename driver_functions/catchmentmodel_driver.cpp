b7e21f2c877039c07e3d2b104c6be4fbe3386c68
Declan Valters <decvalts@users.noreply.github.com>
2015-01-25 21:45:57 +0000

    Fixed the compilation issues during linking

--- driver_functions/catchmentmodel_driver.cpp
+++ driver_functions/catchmentmodel_driver.cpp
@@ -5,8 +5,6 @@
 #include <python2.7/Python.h>
 #include "../LSDCatchmentModel.hpp"
 
-using namespace std;
-
 bool file_check(string name)
 {
 	struct stat buffer;
@@ -15,43 +13,48 @@ bool file_check(string name)
 
 int main(int argc, char *argv[])
 {
-	stringstream ss;
+	std::stringstream ss;
 	
-	if (argc < 2)
+	if (argc < 3)
 	{
-		cout << "\n###################################################" << endl;
-		cout << "No parameter file supplied" << endl;
-		cout << "You must supply a path and parameter file!" << endl;
-		cout << "see http://www.geos.ed.ac.uk/~smudd/LSDTT_docs/html/" << endl;
-		cout << "LSDCatchmentModel docs for assistance." << endl;
-		cout << "###################################################" << endl;
+		std::cout << "\n###################################################" << std::endl;
+		std::cout << "No parameter file supplied" << std::endl;
+		std::cout << "You must supply a path and parameter file!" << std::endl;
+		std::cout << "see http://www.geos.ed.ac.uk/~smudd/LSDTT_docs/html/" << std::endl;
+		std::cout << "LSDCatchmentModel docs for assistance." << std::endl;
+		std::cout << "###################################################" << std::endl;
 		
 		exit(0);  // Game over, try again.
 	}
 
-	if (argc == 2)
+	if (argc == 3)
 	// Bingo!
 	{
-		string pname = argv[1];
-		string pfname = argv[2];
+		
+		std::string pname = argv[1];
+		std::string pfname = argv[2];
 		// The path name and the parameter file name, respectively.
 		// Remember: argc[0] is the program name that you just typed in to the terminal.
-		LSDCatchmentModel modelrun(pname, pfname);
+		std::cout << "The pathname is: " << pname << " and the paramter file is: " << pfname << std::endl;
 		
-		modelrun.run_components();
+		LSDCatchmentModel modelrun(pname, pfname);
+		std::cout << "The parameters have been successfully ingested and the model initialised" << std::endl;
+		//modelrun.run_components();
 	}
 	
 	else
 	{
-		cout << "\n###################################################" << endl;
-		cout << "Either you have supplied too many arguments" << endl;
-		cout << "or, somehow, you have supplied a negative number" << endl;
-		cout << "of arguments. Specify the path name and the parameter file name only." << endl;
-		cout << "There are no other options with this version yet." << endl;
-		cout << "###################################################" << endl;		
+		std::cout << "\n###################################################" << std::endl;
+		std::cout << "Either you have supplied too many arguments" << std::endl;
+		std::cout << "or, somehow, you have supplied a negative number" << std::endl;
+		std::cout << "of arguments. Specify the path name and the parameter file name only." << std::endl;
+		std::cout << "There are no other options with this version yet." << std::endl;
+		std::cout << "###################################################" << std::endl;	
+		
+		exit(0);	
 	}
 
-	cout << "The model ran successfully!" << endl;
-	cout << "SCIENCE IS DONE" << endl;
+	std::cout << "The model ran successfully!" << std::endl;
+	std::cout << "SCIENCE IS DONE" << std::endl;
 	return 0;
 }
