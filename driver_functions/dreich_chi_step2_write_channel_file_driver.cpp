1721d4c700043ae49fe991c9d19a6f5c5a3d925d
Fiona Clubb <fclubb@users.noreply.github.com>
2015-02-26 16:25:56 +0000

    updated channel file writer to take a list of floodplain points, snap to channel, find nearest junction and write channel profile for each one. These profiles will be used to run chi analysis for each junction

--- driver_functions/dreich_chi_step2_write_channel_file_driver.cpp
+++ driver_functions/dreich_chi_step2_write_channel_file_driver.cpp
@@ -10,6 +10,9 @@
 // are in .flt format rather than .bil.  The format of the DEM is now declared in line 2 of the
 // driver file without the first "." e.g. "flt" or "bil"
 //
+// Updated to take a list of floodplain initiation points (X Y coordinates), snap them to the nearest
+// channel, get upstream junction and then write channel file for each junction.
+//
 //
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 //
@@ -75,7 +78,6 @@ int main (int nNumberofArgs,char *argv[])
 	string fill_ext = "_fill";
 	string raster_extension;
 	file_info_in >> DEM_name >> raster_extension;
-	int junction_number;
 	float pruning_threshold;
 	//int threshold;
 	float A_0;
@@ -93,14 +95,13 @@ int main (int nNumberofArgs,char *argv[])
 	float area_thin_frac;
 	int target_skip;
 
-	file_info_in >> Minimum_Slope >> junction_number
-				>> pruning_threshold >> A_0 >> minimum_segment_length >> sigma >> start_movern
+	file_info_in >> Minimum_Slope >> pruning_threshold >> A_0 >> minimum_segment_length >> sigma >> start_movern
 				>> d_movern >> n_movern >> target_nodes >> n_iterations >> fraction_dchi_for_variation
 				>> vertical_interval >> horizontal_interval >> area_thin_frac >> target_skip;
 
 
 	cout << "Paramters of this run: " << endl
-	     << "junction number: " << junction_number << endl
+	     //<< "junction number: " << junction_number << endl
 	     << "pruning_threshold: " << pruning_threshold << endl
 	     << "A_0: " << A_0 << endl
 	     << "minimum_segment_length: " << minimum_segment_length << endl
@@ -117,9 +118,9 @@ int main (int nNumberofArgs,char *argv[])
 	     << "target_skip is: " << target_skip << endl;
 
 
-	string jn_name = itoa(junction_number);
-	string uscore = "_";
-	jn_name = uscore+jn_name;
+// 	string jn_name = itoa(junction_number);
+// 	string uscore = "_";
+// 	jn_name = uscore+jn_name;
 	file_info_in.close();
 
 	string DEM_f_name = path_name+DEM_name+fill_ext;
@@ -148,28 +149,84 @@ int main (int nNumberofArgs,char *argv[])
 
 	// now get the junction network
 	LSDJunctionNetwork ChanNetwork(sources, FlowInfo);
+	
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+//                  Get the floodplain initiation points from file
+// 
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=	
+  
+  cout << "Reading in the floodplain initiation points" << endl;
+  // reading in the floodplain points - getting downstream junctions and then extracting basins
+  string string_filename;
+	string filename = "_floodplain";
+	string dot = ".";
+	string extension = "txt";
+  string_filename = DEM_name+filename+dot+extension;
+  ifstream coords_list;
+  coords_list.open(string_filename.c_str());
+  
+  float X_coord, Y_coord;
+  vector<float> X_coords;
+  vector<float> Y_coords;
+  while (coords_list >> X_coord >> Y_coord)
+  {
+     cout << "X Coord: " << X_coord << " Y Coord: " << Y_coord << endl;
+     X_coords.push_back(X_coord);
+     Y_coords.push_back(Y_coord);
+  }
+  
+  // Snap the floodplain points to the nearest channel
+  cout << "Snapping FIPs to channel; getting junctions" << endl;
+  int threshold_SO = 1;
+  int search_radius = 10;
+  vector<int> snapped_junction_indices;
+  for (unsigned int i = 0; i < X_coords.size(); i++)
+  {
+    cout << flush << "Snapped = " << i+1 << " of " << X_coords.size() << "\r";
+    int NICosmo = ChanNetwork.get_nodeindex_of_nearest_channel_for_specified_coordinates(X_coords[i], Y_coords[i],
+                                                                search_radius, threshold_SO, FlowInfo);
 
-	// now get a junction and look for the longest channel upstream
-	cout << "creating main stem" << endl;
-	LSDIndexChannel main_stem = ChanNetwork.generate_longest_index_channel_in_basin(junction_number, FlowInfo, DistanceFromOutlet);
-        cout << "got main stem channel, with n_nodes " << main_stem.get_n_nodes_in_channel() <<  endl;
-
-	string Basin_name = "_basin_dreich";
-	LSDIndexRaster BasinArray = ChanNetwork.extract_basin_from_junction(junction_number,junction_number,FlowInfo);
-	BasinArray.write_raster((path_name+DEM_name+Basin_name+jn_name),raster_extension);
-
-	// now get the best fit m over n for all the tributaries
-	int organization_switch = 1;
-	int pruning_switch = 1;
-	LSDIndexChannelTree ChannelTree(FlowInfo, ChanNetwork, junction_number, organization_switch,
-                                        DistanceFromOutlet, pruning_switch, pruning_threshold);
-
-	// print a file that can be ingested bt the chi fitting algorithm
-	string Chan_fname = "_ChanNet_dreich";
-	string Chan_ext = ".chan";
-	string Chan_for_chi_ingestion_fname = path_name+DEM_name+Chan_fname+jn_name+Chan_ext;
-	ChannelTree.print_LSDChannels_for_chi_network_ingestion(FlowInfo,
-                             filled_topo_test, DistanceFromOutlet, Chan_for_chi_ingestion_fname);
-	ChannelTree.convert_chan_file_for_ArcMap_ingestion(Chan_for_chi_ingestion_fname);
-
+    int JunctionCosmo = ChanNetwork.find_upstream_junction_from_channel_nodeindex(NICosmo, FlowInfo);
+    snapped_junction_indices.push_back(JunctionCosmo);  
+  }  
+  
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+//                     Write channel file for each junction
+// 
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=	  
+            
+  for (int i=0; i < snapped_junction_indices.size(); i++)
+  {
+    int junction_number = snapped_junction_indices[i];
+    cout << "Junction of FIP: " << junction_number << endl;
+    
+    string jn_name = itoa(junction_number);
+   	string uscore = "_";
+  	jn_name = uscore+jn_name;
+  
+  	// now get a junction and look for the longest channel upstream
+  	cout << "creating main stem" << endl;
+  	LSDIndexChannel main_stem = ChanNetwork.generate_longest_index_channel_in_basin(junction_number, FlowInfo, DistanceFromOutlet);
+          cout << "got main stem channel, with n_nodes " << main_stem.get_n_nodes_in_channel() <<  endl;
+  
+  	string Basin_name = "_basin_dreich";
+  	LSDIndexRaster BasinArray = ChanNetwork.extract_basin_from_junction(junction_number,junction_number,FlowInfo);
+  	BasinArray.write_raster((path_name+DEM_name+Basin_name+jn_name),raster_extension);
+  
+  	// now get the best fit m over n for all the tributaries
+  	int organization_switch = 1;
+  	int pruning_switch = 1;
+  	LSDIndexChannelTree ChannelTree(FlowInfo, ChanNetwork, junction_number, organization_switch,
+                                          DistanceFromOutlet, pruning_switch, pruning_threshold);
+  
+  	// print a file that can be ingested bt the chi fitting algorithm
+  	string Chan_fname = "_ChanNet_dreich";
+  	string Chan_ext = ".chan";
+  	string Chan_for_chi_ingestion_fname = path_name+DEM_name+Chan_fname+jn_name+Chan_ext;
+  	ChannelTree.print_LSDChannels_for_chi_network_ingestion(FlowInfo,
+                               filled_topo_test, DistanceFromOutlet, Chan_for_chi_ingestion_fname);
+  	ChannelTree.convert_chan_file_for_ArcMap_ingestion(Chan_for_chi_ingestion_fname);
+  }
 }
