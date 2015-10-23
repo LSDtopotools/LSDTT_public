d828510f09a9d38aa02ef0c0381e4c6f178dfe3f
Fiona Clubb <fclubb@users.noreply.github.com>
2015-10-23 11:28:34 +0000

    modified function to remove hillslope patches which are connected to first order streams

--- LSDJunctionNetwork.cpp
+++ LSDJunctionNetwork.cpp
@@ -4958,7 +4958,7 @@ LSDIndexRaster LSDJunctionNetwork::remove_hillslope_patches_from_floodplain_mask
       if (FloodplainPatches.get_data_element(row, col) != NoDataValue)
       {
       //check if the pixel is part of the channel network
-        if (StreamOrderArray[row][col] > 0)
+        if (StreamOrderArray[row][col] > 1)
         {
           patch_ids_channel.push_back(FloodplainPatches.get_data_element(row,col));
         }
