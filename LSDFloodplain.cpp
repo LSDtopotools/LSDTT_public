6769ed17624f1269017464a054adc416dba7300a
Fiona Clubb <fclubb@users.noreply.github.com>
2016-10-18 15:54:00 +0000

    Adding LSDFloodplain class

---
+++ LSDFloodplain.cpp
@@ -0,0 +1,239 @@
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// LSDFloodplain.cpp
+//
+// Land Surface Dynamics Floodplain Object
+//
+// This object creates and stores information about floodplain and terraces
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// An object within the University
+//  of Edinburgh Land Surface Dynamics group topographic toolbox
+//
+// Developed by:
+//  Simon M. Mudd
+//  Martin D. Hurst
+//  David T. Milodowski
+//  Stuart W.D. Grieve
+//  Declan A. Valters
+//  Fiona Clubb
+//
+// Copyright (C) 2013 Simon M. Mudd 2013
+//
+// Developer can be contacted by simon.m.mudd _at_ ed.ac.uk
+//
+//    Simon Mudd                                                    
+//    University of Edinburgh
+//    School of GeoSciences
+//    Drummond Street
+//    Edinburgh, EH8 9XP
+//    Scotland
+//    United Kingdom
+//
+// This program is free software;
+// you can redistribute it and/or modify it under the terms of the
+// GNU General Public License as published by the Free Software Foundation;
+// either version 2 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY;
+// without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the
+// GNU General Public License along with this program;
+// if not, write to:
+// Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor,
+// Boston, MA 02110-1301
+// USA
+//
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+#ifndef LSDFloodplain_CPP
+#define LSDFloodplain_CPP
+
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include "TNT/tnt.h"
+#include "LSDFlowInfo.hpp"
+#include "LSDRaster.hpp"
+#include "LSDIndexRaster.hpp"
+#include "LSDIndexChannel.hpp"
+#include "LSDJunctionNetwork.hpp"
+#include "LSDStatsTools.hpp"
+#include "LSDFloodplain.hpp"
+using namespace std;
+using namespace TNT;
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// create
+// this populates the binary array and connected components array for the floodplain
+// given rasters of channel relief and slope and thresholds for both. Each pixel
+// must be below the slope and channel relief threshold to be classified as floodplain.
+// User must set a minimum floodplain patch size (in pixels, set to 0 if all patches are kept).
+//
+// FJC 18/10/16
+//
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+void LSDFloodplain::create(LSDRaster& ChannelRelief, LSDRaster& Slope, float relief_thresh, float slope_thresh, int min_patch_size)
+{	
+	
+  /// set the protected variables
+	NRows = ChannelRelief.get_NRows();
+	NCols = ChannelRelief.get_NCols();
+	XMinimum = ChannelRelief.get_XMinimum();
+	YMinimum = ChannelRelief.get_YMinimum();
+	DataResolution = ChannelRelief.get_DataResolution();
+	NoDataValue = ChannelRelief.get_NoDataValue();
+	GeoReferencingStrings = ChannelRelief.get_GeoReferencingStrings();
+	
+	relief_threshold = relief_thresh;
+	slope_threshold = slope_thresh;
+	
+	//declare the arrays
+	Array2D<int> TempBinArray(NRows,NCols,0);
+	Array2D<int> TempLinkArray(NRows,NCols,NoDataValue);
+	BinaryArray = TempBinArray.copy();
+	ConnectedComponents_Array = TempLinkArray.copy();
+	
+	//loop through every row and col and get the slope and relief values
+  for (int i =0; i < NRows; i++)
+  {
+    for (int j = 0; j < NCols; j++)
+    {
+      if (ChannelRelief.get_data_element(i,j) != NoDataValue && Slope.get_data_element(i,j) != NoDataValue)
+      {
+        float slope = Slope.get_data_element(i,j);
+        float relief = ChannelRelief.get_data_element(i,j);
+        if (relief < relief_threshold && slope < slope_threshold)        //floodplain points must be lower than both the relief
+        {                                                                //and the slope threshold.
+          BinaryArray[i][j] = 1;
+        }
+      }
+    }
+  }
+	LSDIndexRaster FloodplainRaster(NRows,NCols,XMinimum,YMinimum,DataResolution,NoDataValue,BinaryArray,GeoReferencingStrings);
+	
+	// run the connected components algorithm on the floodplain array
+	LSDIndexRaster ConnectedComponents = FloodplainRaster.ConnectedComponents();
+	if (min_patch_size > 0)
+	{
+		LSDIndexRaster ConnectedComponents_final = ConnectedComponents.RemoveSmallPatches(min_patch_size);
+		ConnectedComponents_Array = ConnectedComponents_final.get_RasterData();
+	}
+	else
+	{		
+		ConnectedComponents_Array = ConnectedComponents.get_RasterData();	
+	}
+
+}
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Takes in a junction number and generates the main stem channel from this point
+// The information about each floodplain or terrace pixel is then calculated relative
+// to the main channel.
+// FJC 18/10/16
+//
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+void LSDFloodplain::get_main_stem_information(int junction_number, LSDJunctionNetwork& ChanNetwork, LSDFlowInfo& FlowInfo, LSDRaster& DistFromOutlet, LSDRaster& ElevationRaster)
+{
+	// set protected variables
+	JunctionNumber = junction_number;
+	
+	// get the main stem channel from this junction
+	LSDIndexChannel MainStem = ChanNetwork.generate_longest_index_channel_from_junction(JunctionNumber, FlowInfo, DistFromOutlet);
+	int downstream_node = ChanNetwork.get_Node_of_Junction(JunctionNumber);
+	UpslopeNodes = FlowInfo.get_upslope_nodes(downstream_node);
+	cout << "There are: " << UpslopeNodes.size() << " nodes from this junction." << endl;
+	
+	// loop through all the upslope nodes and find ones that are in the connected components raster
+	for (int i = 0; i < int(UpslopeNodes.size()); i++)
+	{
+		int row, col;
+		FlowInfo.retrieve_current_row_and_col(UpslopeNodes[i], row, col);
+		if (ConnectedComponents_Array[row][col] != NoDataValue)
+		{
+			int ChannelNode;
+			float FlowLength, DistanceUpstream, Relief;
+			ChanNetwork.get_info_nearest_channel_to_node_main_stem(UpslopeNodes[i], FlowInfo, ElevationRaster, DistFromOutlet, MainStem, ChannelNode, FlowLength, DistanceUpstream, Relief);
+			// populate arrays with relief and distance
+			MainStemNIs[row][col] = ChannelNode;
+			ChannelRelief_array[row][col] = Relief;
+			UpstreamDistance_array[row][col] = DistanceUpstream;
+			//cout << "Relief: " << Relief << " Upstream dist: " << DistanceUpstream << endl;
+		}
+	}		
+}
+
+//----------------------------------------------------------------------------------------
+// This function separates the floodplain into floodplain patches and terrace patches depending
+// on whether they are connected to the channel network.
+// FJC 18/10/16
+//---------------------------------------------------------------------------------------- 
+void LSDFloodplain::separate_floodplain_and_terrace_patches(LSDJunctionNetwork& ChanNetwork, float threshold_SO, LSDIndexRaster& FloodplainPatches, LSDIndexRaster& TerracePatches)
+{
+  Array2D<int> FloodplainPatches_array(NRows,NCols,NoDataValue);
+	Array2D<int> TerracePatches_array(NRows,NCols,NoDataValue);
+  vector<int> patch_ids_channel;
+	Array2D<int> StreamOrderArray = ChanNetwork.get_StreamOrderArray();
+  
+  //loop through the DEM and get the ID of all patches connected to the channel network
+  for (int row = 0; row < NRows; row++)
+  {
+    for (int col = 0; col < NCols; col++)
+    {
+      if (ConnectedComponents_Array[row][col] != NoDataValue)
+      {
+      //check if the pixel is part of the channel network
+        if (StreamOrderArray[row][col] >= threshold_SO)
+        {
+          patch_ids_channel.push_back(ConnectedComponents_Array[row][col]);
+        }
+      }
+    }
+  }
+  
+  //for each pixel, find if it is in a patch with an ID in patch_ids_channel vector
+  vector<int>::iterator find_it;
+  for (int row = 0; row < NRows; row++)
+  {
+    for (int col = 0; col < NCols; col++)
+    {
+      if (ConnectedComponents_Array[row][col] != NoDataValue)
+      {
+        int patch_id = ConnectedComponents_Array[row][col];
+        find_it = find(patch_ids_channel.begin(), patch_ids_channel.end(), patch_id);   //search ID vector for patch ID of pixel
+        if (find_it != patch_ids_channel.end())
+        {
+          FloodplainPatches_array[row][col] = patch_id;                
+        }
+				else
+				{
+					TerracePatches_array[row][col] = patch_id;
+				}
+      }      
+    }
+  }
+  
+  //get the LSDIndexRaster from floodplain patches array
+  LSDIndexRaster FloodplainPatches_temp(NRows,NCols, XMinimum, YMinimum, DataResolution, NoDataValue, FloodplainPatches_array, GeoReferencingStrings);
+	
+	LSDIndexRaster TerracePatches_temp(NRows,NCols, XMinimum, YMinimum, DataResolution, NoDataValue, TerracePatches_array, GeoReferencingStrings);
+	
+	//copy to rasters
+	FloodplainPatches = FloodplainPatches_temp;
+	TerracePatches = TerracePatches_temp;
+   
+}
+
+
+
+#endif
---
+++ LSDFloodplain.hpp
@@ -0,0 +1,112 @@
+//beginning of the LSDFloodplain class
+
+#ifndef LSDFloodplain_H
+#define LSDFloodplain_H
+
+#include <vector>
+#include <string>
+#include <algorithm>
+#include "TNT/tnt.h"
+#include "LSDFlowInfo.hpp"
+#include "LSDRaster.hpp"
+#include "LSDIndexRaster.hpp"
+#include "LSDJunctionNetwork.hpp"
+#include "LSDStatsTools.hpp"
+using namespace std;
+using namespace TNT;
+
+///@brief Class to store information about floodplains and terraces...
+class LSDFloodplain
+{
+  public:
+
+  /// @brief create function
+  ///
+  /// @details This populates the binary array and connected components array for the floodplain
+	/// given rasters of channel relief and slope and thresholds for both. Each pixel
+  /// must be below the slope and channel relief threshold to be classified as floodplain.
+  /// @author FJC
+	/// 18/10/16
+  LSDFloodplain(LSDRaster& ChannelRelief, LSDRaster& Slope, float relief_threshold, float slope_threshold, int min_patch_size)
+					{ create(ChannelRelief, Slope, relief_threshold, slope_threshold, min_patch_size); }
+	
+	/// @return Number of rows as an integer.
+  int get_NRows() const        { return NRows; }
+  /// @return Number of columns as an integer.
+  int get_NCols() const        { return NCols; }
+  /// @return Minimum X coordinate as an integer.
+  float get_XMinimum() const        { return XMinimum; }
+  /// @return Minimum Y coordinate as an integer.
+  float get_YMinimum() const        { return YMinimum; }
+  /// @return Data resolution as an integer.
+  float get_DataResolution() const        { return DataResolution; }
+  /// @return No Data Value as an integer.
+  int get_NoDataValue() const        { return NoDataValue; }
+	/// @return Georeferencing information
+  map<string,string> get_GeoReferencingStrings() const { return GeoReferencingStrings; }  
+	
+	/// @brief This function gets the information about all the floodplain pixels connected to the main stem channel from a junction
+	/// @details Takes a junction number and generates the main stem channel from this point. THe information about each floodplain or terrace pixel is then calculated relative to the main channel.
+	/// @param junction_number junction number of interest
+	/// @param ChanNetwork LSDJunctionNetwork object
+	/// @param FlowInfo LSDFlow info object
+	/// @param DistFromOutlet LSDRaster of distances from the outlet
+	/// @param ElevationRaster LSDRaster of elevations
+	/// @author FJC
+	/// @date 18/10/16
+	void get_main_stem_information(int junction_number, LSDJunctionNetwork& ChanNetwork, LSDFlowInfo& FlowInfo, LSDRaster& DistFromOutlet, LSDRaster& ElevationRaster);
+	
+	/// @brief This function separates te floodplain into floodplain patches and terrace patches depending 
+	/// on whether they are connected to the channel network.
+	/// @param ChanNetwork LSDJunctionNetwork object
+	/// @param threshold_SO threshold SO to count as the channel network
+	/// @param FloodplainPatches Empty LSDIndexRaster to store floodplain patches
+	/// @param TerracePatches Empty LSDIndexRaster to store terrace patches
+	/// @author FJC
+	/// @date 18/10/16
+	void separate_floodplain_and_terrace_patches(LSDJunctionNetwork& ChanNetwork, float threshold_SO, LSDIndexRaster& FloodplainPatches, LSDIndexRaster& TerracePatches);
+
+  protected:
+	
+	/// Number of rows
+	int NRows;
+	/// Number of columns
+	int NCols;
+	/// X minimum
+	float XMinimum;
+	/// Y minimum
+	float YMinimum;
+	/// Data resolution
+	float DataResolution;
+	/// No data value
+	int NoDataValue;
+	/// A map of strings for holding georeferencing information
+  map<string,string> GeoReferencingStrings;
+		
+	/// Relief threshold
+	float relief_threshold;
+	/// Slope threshold
+	float slope_threshold;
+	
+  /// The binary array of floodplain data
+	Array2D<int> BinaryArray;
+	/// The array of connected components
+	Array2D<int> ConnectedComponents_Array;
+	
+	/// Junction number - used to calculate information about the patches compared to the main stem channel
+	int JunctionNumber;
+	/// vector of nodes upslope of the junction
+	vector<int> UpslopeNodes;
+	/// array of the nearest NI on the main stem to each pixel
+	Array2D<int> MainStemNIs;
+	/// array of relief relative to main stem
+	Array2D<float> ChannelRelief_array;
+	/// array of distance upstream along main stem
+	Array2D<float> UpstreamDistance_array;
+	
+  private:
+	void create(LSDRaster& ChannelRelief, LSDRaster& Slope, float relief_threshold, float slope_threshold, int min_patch_size);
+
+};
+
+#endif
