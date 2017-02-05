ded594a614f852d3d2e995ec4e66b79fd01f45d0
Martin Hurst <mhurst@geodesy.geog.gla.ac.uk>
2017-02-05 16:06:59 +0000

    tested and working version of basin averaging driver

--- driver_functions_MDH/basin_averaged_topo_metrics.cpp
+++ driver_functions_MDH/basin_averaged_topo_metrics.cpp
@@ -74,38 +74,22 @@ int main (int nNumberofArgs,char *argv[])
 	//Test for correct input arguments
 	if (nNumberofArgs!=3)
 	{
-		cout << "FATAL ERROR: wrong number inputs. The program needs the path name and the file name" << endl;
+		cout << "FATAL ERROR: wrong number inputs. The program needs the path name and the DEM name" << endl;
 		exit(EXIT_SUCCESS);
 	}
 
 	string path_name = argv[1];
-	string f_name = argv[2];
+	string DEM_name = argv[2];
 
-	cout << "The path is: " << path_name << " and the filename is: " << f_name << endl;
-
-	string full_name = path_name+f_name;
-
-	ifstream file_info_in;
-	file_info_in.open(full_name.c_str());
-	if( file_info_in.fail() )
-	{
-		cout << "\nFATAL ERROR: the header file \"" << full_name
-		     << "\" doesn't exist" << endl;
-		exit(EXIT_FAILURE);
-	}
+	cout << "The path is: " << path_name << " and the DEM name is: " << DEM_name << endl;
 
   // get some file names
-	string DEM_name;
-	string CHeads_file;
-	string fill_ext = "_fill";
-	file_info_in >> DEM_name >> CHeads_file;
-	float Minimum_Slope;
-	file_info_in >> Minimum_Slope;
+	string fill_ext = "_Fill";
 	string DEM_f_name = path_name+DEM_name+fill_ext;
 	string FileExtension = "bil";
 
   // Get list of junctions into a vector - this file is produced by ./drainage_density_step1_junctions.out
-  string ListOfJunctions = DEM_name+"_junctions.txt";
+  string ListOfJunctions = DEM_name+"_junctions.list";
   vector<int> junction_list;
   int this_junction;
   string line;
@@ -140,7 +124,7 @@ int main (int nNumberofArgs,char *argv[])
 	//create stream network from mapped sources
 	cout << "\t Loading Sources..." << endl;
 	vector<int> sources;
-	CHeads_file = path_name+DEM_name+"_CH_wiener_nodeindices_for_Arc";
+	string CHeads_file = path_name+DEM_name+"_CH_wiener_nodeindices_for_Arc";
 	string CHeads_file_ext = CHeads_file + ".csv";
 	if (ifstream(CHeads_file_ext.c_str()))
 	{
@@ -187,7 +171,7 @@ int main (int nNumberofArgs,char *argv[])
 	LSDRaster CHT((path_name+DEM_name+CHT_ext), FileExtension);
 	
 	//get the LH raster
-	string LH_ext = "HFR_LH";
+	string LH_ext = "_HFR_LH";
 	LSDRaster LH((path_name+DEM_name+LH_ext), FileExtension);
 
 	//get the S raster
