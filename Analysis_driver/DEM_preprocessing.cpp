5355b068295d29bb0837e95acbf636a8eddf1619
Simon M. Mudd <simon.m.mudd@ed.ac.uk>
2017-03-17 11:14:19 +0000

    Got rid of basin routines in DEM preprocessing since those are in the basin_averager

--- Analysis_driver/DEM_preprocessing.cpp
+++ Analysis_driver/DEM_preprocessing.cpp
@@ -51,59 +51,6 @@
 #include "../LSDShapeTools.hpp"
 using namespace std;
 
-//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-// This function prints the basins and an additional file that has basin centroids
-// and labelling information for plotting
-//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-void print_basins(string basin_raster_name, LSDFlowInfo& FlowInfo, LSDJunctionNetwork& JunctionNetwork,
-                               vector<int> Junctions)
-{
-  int N_Juncs = Junctions.size();
-  LSDCoordinateConverterLLandUTM Converter;
-
-
-  // Get some data members for holding basins and the raster
-  vector<LSDBasin> AllTheBasins;
-  map<int,int> drainage_of_other_basins;
-  LSDIndexRaster BasinMasterRaster;
-
-  //cout << "I am trying to print basins, found " << N_BaseLevelJuncs << " base levels." << endl;
-  // Loop through the junctions
-  for(int BN = 0; BN<N_Juncs; BN++)
-  {
-    //cout << "Getting basin " << BN << " and the junction is: "  << BaseLevelJunctions[BN] << endl;
-    LSDBasin thisBasin(Junctions[BN],FlowInfo, JunctionNetwork);
-    //cout << "...got it!" << endl;
-    AllTheBasins.push_back(thisBasin);
-
-    // This is required if the basins are nested--test the code which numbers
-    // to be overwritten by a smaller basin
-    drainage_of_other_basins[Junctions[BN]] = thisBasin.get_NumberOfCells();
-  }
-
-  // now loop through everything again getting the raster
-  if (N_Juncs > 0)     // this gets the first raster
-  {
-    BasinMasterRaster = AllTheBasins[0].write_integer_data_to_LSDIndexRaster(Junctions[0], FlowInfo);
-  }
-
-  // now add on the subsequent basins
-  for(int BN = 1; BN<N_Juncs; BN++)
-  {
-    AllTheBasins[BN].add_basin_to_LSDIndexRaster(BasinMasterRaster, FlowInfo,
-                              drainage_of_other_basins, Junctions[BN]);
-  }
-
-
-  // We need to use bil format since there is georeferencing
-  string raster_ext = "bil";
-  // print the basin raster
-  BasinMasterRaster.write_raster(basin_raster_name, raster_ext);
-  cout << "Finished with exporting basins!" << endl;
-
-}
-
-
 int main (int nNumberofArgs,char *argv[])
 {
 
@@ -154,21 +101,12 @@ int main (int nNumberofArgs,char *argv[])
   float_default_map["relief_radius"] = 100;
   float_default_map["relief_threshold"] = 50;
 
-  // some parameters for snapping to basins
-  int_default_map["threshold_contributing_pixels"] = 1000;
-  int_default_map["search_radius_nodes"] = 10;
-  string_default_map["basin_outlet_csv"] = "NULL";
 
   // set default methods
   bool_default_map["fill_nodata"] = false;
   bool_default_map["remove_low_relief"] = false;
   bool_default_map["write_relief_raster"] = false;
-  bool_default_map["check_basin_locations"] = false;
-  bool_default_map["print_stream_order_raster"] = false;
-  
-  
-  
-  
+
   // Use the parameter parser to get the maps of the parameters required for the 
   // analysis
   LSDPP.parse_all_parameters(float_default_map, int_default_map, bool_default_map,string_default_map);
@@ -248,157 +186,6 @@ int main (int nNumberofArgs,char *argv[])
   
   }
   
-  // Now print the new file
-  string preprocess_str = "_PP";
-  FinalRaster.write_raster(OUT_DIR+OUT_ID+preprocess_str,raster_ext);
-  
-  // now we check for basins (if you've ask for that)
-  if(this_bool_map["check_basin_locations"])
-  {
-    // first we see if the nodes file exists
-    string basin_outlet_fname = DATA_DIR+this_string_map["basin_outlet_csv"];
-    
-    cout << "I am checking basins, the outlet points are: " <<  basin_outlet_fname << endl;
-  
-    ifstream ifs; 
-    ifs.open(basin_outlet_fname.c_str());
-  
-    // check if the parameter file exists
-    if( ifs.fail() )
-    {
-      cout << "\nFATAL ERROR: The basin points file \"" << basin_outlet_fname
-           << "\" doesn't exist" << endl;
-    }
-    else
-    {
-      // read the file
 
-
-      // the LL data
-      vector<float> latitude;
-      vector<float> longitude;
-      vector<string> IDs;
-      string line_from_file;
-      vector<string> this_string_vec;
-      vector<string> empty_string_vec;
-      
-      // now loop through the rest of the lines, getting the data. 
-      while( getline(ifs, line_from_file))
-      {
-        // reset the string vec
-        this_string_vec = empty_string_vec;
-    
-        // create a stringstream
-        stringstream ss(line_from_file);
-    
-        while( ss.good() )
-        {
-          string substr;
-          getline( ss, substr, ',' );
-      
-          // remove the spaces
-          substr.erase(remove_if(substr.begin(), substr.end(), ::isspace), substr.end());
-      
-          // remove control characters
-          substr.erase(remove_if(substr.begin(), substr.end(), ::iscntrl), substr.end());
-      
-          // add the string to the string vec
-          this_string_vec.push_back( substr );
-        }
-    
-        //cout << "Yoyoma! size of the string vec: " <<  this_string_vec.size() << endl;
-        if ( int(this_string_vec.size()) < 3)
-        {    
-          cout << "Hey there, I am trying to load your basin outlet data but you seem not to have" << endl;
-          cout << "enough columns in your file. I am ignoring a line" << endl;
-        }
-        else
-        {
-          // now convert the data
-          //cout << "Getting sample name: " <<  this_string_vec[0] << endl;
-      
-          // let the user know about offending underscores, and replace them
-          string s = this_string_vec[0];
-          string uscore = "_";
-          size_t found = s.find(uscore);
-          if (found!=std::string::npos)
-          {
-            cout << "I found an underscore in the sample name. Replacing with a dash." <<endl;
-            replace( s.begin(), s.end(), '_', '-');
-            cout << "New sample name is: " << s << endl;
-          }
-
-          IDs.push_back( s );
-          latitude.push_back( atof( this_string_vec[1].c_str() ) );
-          longitude.push_back( atof(this_string_vec[2].c_str() ) );
-        }
-      }
-      ifs.close();
-
-      int N_lats = int(latitude.size());
-      cout << "N outlets: " << N_lats << endl;
-      for (int i = 0; i< N_lats; i++)
-      {
-        cout << "lat: " << latitude[i] << " and long: " <<  longitude[i] << endl;
-      }
-
-      // now get a flow info
-  
-      // Fill this raster
-      float fill_slope = 0.0001;
-      cout << "Filling the raster" << endl;
-      LSDRaster filled_raster = FinalRaster.fill(fill_slope);
-  
-      // a boundary condition for the flow info object
-      vector<string> boundary_conditionst(4);
-      boundary_conditionst[0] = "n";
-      boundary_conditionst[1] = "n";
-      boundary_conditionst[2] = "n";
-      boundary_conditionst[3] = "n";
-      boundary_conditions = boundary_conditionst;
-      
-      // get the flow info
-      LSDFlowInfo FlowInfo(boundary_conditions, filled_raster);
-
-      // get contributing pixels (needed for junction network)
-      LSDIndexRaster ContributingPixels = FlowInfo.write_NContributingNodes_to_LSDIndexRaster();
-  
-      // get the sources
-      vector<int> sources;
-      sources = FlowInfo.get_sources_index_threshold(ContributingPixels, this_int_map["threshold_contributing_pixels"]);
-  
-      // now get the junction network
-      vector<float> fUTM_easting;
-      vector<float> fUTM_northing;
-      LSDJunctionNetwork JNetwork(sources, FlowInfo);
-      
-      if (this_bool_map["print_stream_order_raster"])
-      {
-        LSDIndexRaster SOArray = JNetwork.StreamOrderArray_to_LSDIndexRaster();
-        string SO_raster_name = OUT_DIR+OUT_ID+"_SO";
-        SOArray.write_raster(SO_raster_name,raster_ext);
-      }
-      
-      JNetwork.get_x_and_y_from_latlong(latitude, longitude,fUTM_easting,fUTM_northing);
-
-      int threshold_stream_order = 2;
-      vector<int> valid_cosmo_points;         // a vector to hold the valid nodes
-      vector<int> snapped_node_indices;       // a vector to hold the valid node indices
-      vector<int> snapped_junction_indices;   // a vector to hold the valid junction indices
-      cout << "The search radius is: " << this_int_map["search_radius_nodes"] << endl;
-      JNetwork.snap_point_locations_to_channels(fUTM_easting, fUTM_northing, 
-                this_int_map["search_radius_nodes"], threshold_stream_order, FlowInfo, 
-                valid_cosmo_points, snapped_node_indices, snapped_junction_indices);
-      
-      cout << "The number of valid points is: " << int(valid_cosmo_points.size()) << endl;
-      
-      // Now get the basin rasters
-      string basin_raster_name = OUT_DIR+OUT_ID+"_AllBasins";
-      print_basins(basin_raster_name, FlowInfo, JNetwork,
-                   snapped_junction_indices);
-    }
-  }
-  
-  
   
 }
