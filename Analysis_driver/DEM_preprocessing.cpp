b247cf21389cc17416e33028187e851793491c0a
Simon M. Mudd <simon.m.mudd@ed.ac.uk>
2017-03-17 15:24:56 +0000

    Added some functions to find holes in a raster

--- Analysis_driver/DEM_preprocessing.cpp
+++ Analysis_driver/DEM_preprocessing.cpp
@@ -186,6 +186,19 @@ int main (int nNumberofArgs,char *argv[])
   
   }
   
+  
+  if(this_bool_map["find_holes"])
+  {
+    int const_value = 0;
+    LSDIndexRaster(FinalRaster,0)
+  
+  
+  }
+  
+  
+  
+  
+  
   // Now write the final raster
   string preprocess_str = "_PP";
   FinalRaster.write_raster(OUT_DIR+OUT_ID+preprocess_str,raster_ext);
