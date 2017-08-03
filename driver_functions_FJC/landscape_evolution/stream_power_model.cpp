5b5b8e68fded5348c676587da01e592c7ac66928
fclubb <f.clubb@ed.ac.uk>
2017-08-03 14:24:57 +0100

    Fixing bug in model script

--- driver_functions_FJC/landscape_evolution/stream_power_model.cpp
+++ driver_functions_FJC/landscape_evolution/stream_power_model.cpp
@@ -1,9 +1,18 @@
+//-=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=-//
+// stream_power_model.cpp
+// This script runs a simple stream power model using detachment-limited
+// stream power with the Fastscape algorithm
+//
+// FJC
+// 03/08/17
+//-=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=-//
+
 #include <iostream>
 #include <sstream>
 #include <cstdlib>
 #include <sys/stat.h>
 #include <python2.7/Python.h>
-#include "../LSDRasterModel.hpp"
+#include "../../LSDRasterModel.hpp"
 
 using namespace std;
 
@@ -19,10 +28,10 @@ int main(int argc, char *argv[])
 	LSDRasterModel mod;
 	stringstream ss;
 	string pathname;
-	
+
 	if (argc <= 1)
 	{
-	  cout << "ERROR: you need to enter a data folder and, \n" 
+	  cout << "ERROR: you need to enter a data folder and, \n"
          << "optionally, a parameter name and a run name" << endl;
     exit(0);
   }
@@ -32,14 +41,14 @@ int main(int argc, char *argv[])
     string lchar = pathname.substr(pathname.length()-2,1);
     string slash = "/";
     cout << "lchar is " << lchar << " and slash is " << slash << endl;
-      
+
     if (lchar != slash)
     {
-      cout << "You forgot the frontslash at the end of the path. Appending." << endl;  
+      cout << "You forgot the frontslash at the end of the path. Appending." << endl;
       pathname = pathname+slash;
-    } 
+    }
     cout << "The pathname is: " << pathname << endl;
-  }  
+  }
   if (argc >2 )
   {
     string param_name = argv[2];
@@ -47,7 +56,7 @@ int main(int argc, char *argv[])
     string full_param_name = pathname+param_name;
     cout << "The full path is: " << full_param_name << endl;
     mod.initialize_model(full_param_name);
-    
+
     // add the path to the default filenames
 		mod.add_path_to_names( pathname);
   }
@@ -64,16 +73,16 @@ int main(int argc, char *argv[])
 
     // set the end time, print interval, etc
     mod.set_K(0.0001);
-    mod.set_endTime(50000);  
+    mod.set_endTime(50000);
     mod.set_print_interval(25);
 
 		string template_param = "template_param.param";
-		string full_template_name = pathname+template_param;		
+		string full_template_name = pathname+template_param;
 		mod.make_template_param_file(full_template_name);
-		
+
 		// add the path to the default filenames
 		mod.add_path_to_names( pathname);
-		
+
 		// add random asperities to the surface of default model
 		mod.random_surface_noise();
 	}
@@ -92,7 +101,7 @@ int main(int argc, char *argv[])
 			string ans;
 			cerr << "A run with the name '" << mod.get_name() << "' already exsist, do you wish to overwrite it? (y/n) ";
 			cin >> ans;
-			if (ans != "y") 
+			if (ans != "y")
       {
         cout << "You will need to choose another run name, exiting" << endl;
         exit(0);
@@ -111,7 +120,7 @@ int main(int argc, char *argv[])
 
 	mod.print_parameters();
 
-  // run the model to steady state. 
+  // run the model to steady state.
   // first change the default dimensions
   //int newrows = 300;
   //int newcols = 150;
@@ -119,21 +128,21 @@ int main(int argc, char *argv[])
 
   // and turn the hillslopes off, just to save some time
   mod.set_hillslope(false);
-  
+
   // now run to steady state
 	mod.reach_steady_state();
-	
+
 	// and just to make sure, run it a bit longer
 	mod.run_components_combined();
-	
+
 	// now print the slope area data
 	int area_flag = 1;       // use d8 (like current implementation of FASTSCAPE)
 	int slope_flag = 1;      // use d8 (like FASTSCAPE does)
 	string SA_ext = "_SA.slopearea";
 	string SA_fname = mod.get_name()+SA_ext;
-  
+
   mod.slope_area_data(SA_fname,slope_flag,area_flag);
-	
+
 	return 0;
-	
+
 }
