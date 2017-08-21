154581949a3df74fe91b11cc7729c53bb0e3c3a9
simon-m-mudd <simon.m.mudd@ed.ac.uk>
2017-08-21 10:50:20 +0100

    Working on the MuddPILEdriver files.

--- driver_functions_SMM_model/MuddPILEdriver.cpp
+++ driver_functions_SMM_model/MuddPILEdriver.cpp
@@ -40,6 +40,7 @@
 
 #include <sstream>
 #include <cstdlib>
+#include <cmath>
 #include <sys/stat.h>
 #include "../LSDRasterModel.hpp"
 #include "../LSDParticleColumn.hpp"
@@ -94,10 +95,21 @@ int main (int nNumberofArgs,char *argv[])
   int_default_map["NCols"] = 400;
   float_default_map["DataResolution"] = 30;
   
+  // Parameters for the initial surface
+  bool_default_map["use_diamond_square_initial"] = true;
+  float_default_map["diamond_square_relief"] = 10;
+  int_default_map["diamond_square_feature_order"] = 8;
+  bool_default_map["superimpose_parabola"] = true;
+  float_default_map["parabola_relief"] = 10;
+  bool_default_map["roughen_surface"] = true;
+  float_default_map["roughness_relief"] = 0.25;
+  bool_default_map["taper_edges"] = true;
+  int_default_map["taper_rows"] = 10;
   
   // Parameters for spinning up the simulation
   float_default_map["spinup_K"] = 0.001;
   float_default_map["spinup_time"] = 50000;
+  bool_default_map["staged_spinup"] = false;
   
   
   // control of how long you want the simulations to run
@@ -131,7 +143,6 @@ int main (int nNumberofArgs,char *argv[])
   cout << "Write filename is: " << OUT_DIR+OUT_ID << endl;
 
 
-
   // Initiate the model object
   LSDRasterModel mod;
 
@@ -140,10 +151,12 @@ int main (int nNumberofArgs,char *argv[])
   mod.print_parameters();
 
   // see if we want to load a prior DEM
+  bool create_initial_surface = false;
   if(this_bool_map["read_initial_raster"])
   {
     cout << "I am going to try to read an intial raster for you." << endl;
     cout << "The read filename is: " <<  DATA_DIR+DEM_ID << endl;
+    cout << "I am going to IGNORE initial surface instructions!" << endl;
     string header =  DATA_DIR+DEM_ID+".hdr";
     ifstream file_info_in;
     file_info_in.open(header.c_str());
@@ -158,15 +171,59 @@ int main (int nNumberofArgs,char *argv[])
     else
     {
       cout << "Header doesn't exist. I am switching to a fixed rows and columns." << endl;
-      mod.resize_and_reset(this_int_map["NRows"],this_int_map["NCols"],this_float_map["DataResolution"]);
+      cout << "I will also create an initial surface for you." << endl;
+      create_initial_surface = true;
     }
   }
   else
   {
-    mod.resize_and_reset(this_int_map["NRows"],this_int_map["NCols"],this_float_map["DataResolution"]);
+    cout << "You have chosen not to read an initial raster so I will create an initial surface for you." << endl;
+    create_initial_surface = true;
   }
   
-  // now do the spinup
+  // create an initial surface if needed
+  if(create_initial_surface)
+  {
+    cout << endl << endl << "=============================================" << endl;
+    cout << "Creating and initial surface. " << endl;
+    cout << "NRows: " << this_int_map["NRows"]<< ", NCols: " << this_int_map["NCols"] 
+         << ", DataResolution: " << this_float_map["DataResolution"] << endl;
+    mod.resize_and_reset(this_int_map["NRows"],this_int_map["NCols"],this_float_map["DataResolution"]);
+    
+    if (this_bool_map["use_diamond_square_initial"])
+    {
+      cout << "   I am starting with a fractal surface created by the diamond square algorithm." << endl;
+      cout << "   It has a relief of " << this_int_map["diamond_square_feature_order"] << endl;
+      cout << "   Let me check on the largest possible scale of features in the pseudo fractal surface." << endl;
+      
+      // we need to check the feature order to make sure it is not bigger than the largest dimension of the raster
+      int largest_dimension;
+      if( this_int_map["NRows"] >= this_int_map["NCols"]) 
+      {
+        largest_dimension = this_int_map["NRows"];
+      }      
+      else
+      {
+        largest_dimension = this_int_map["NCols"];
+      }
+      // now get the largest possible dimension
+      float lps = floor( log2( float(largest_dimension) ) );
+      int largest_possible_scale = int(lps);
+      if( this_int_map["diamond_square_feature_order"] > largest_possible_scale )
+      {
+        this_int_map["diamond_square_feature_order"] = largest_possible_scale;
+      }
+      cout << "    The largest dimnesion is: " << largest_dimension << " pixels." << endl;
+      cout << "    So the biggest scale is: " << pow(2,largest_possible_scale) << " pixels or 2 to the " << largest_possible_scale << " power" << endl;
+      cout << "    I am setting the scale to: " << this_int_map["diamond_square_feature_order"] << endl; 
+
+      // Now actually build the fractal surface
+      mod.intialise_diamond_square_fractal_surface(this_int_map["diamond_square_feature_order"], this_float_map["diamond_square_relief"]);
+    }
+    
+    
+  }
+  
 
 
 }
