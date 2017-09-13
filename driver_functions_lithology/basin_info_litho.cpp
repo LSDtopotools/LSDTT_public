6d5374ed75c5f660eb58f72c9948f8b73eb34c13
boris gailleton <boris.gailleton@gmail.com>
2017-09-13 11:40:27 +0200

    I am beginning to work on lithology stuffs. Slowly because my laptop is dying.

---
+++ driver_functions_lithology/basin_info_litho.cpp
@@ -0,0 +1,317 @@
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//
+// basin_info_litho
+//
+// This program takes two arguments, the path name and the driver name
+// The driver file has a number of options to extract basin information
+// function to a lithology/geology map. This last can be pre-processed
+// from a shapefile following the (future) instructions on LSDMappingTool
+// LSD_Geology.py script available on:
+// https://github.com/LSDtopotools/LSDMappingTools
+// *I am working on this now, this is not ready yet - Boris*
+//
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//
+// Copyright (C) 2016 Simon M. Mudd 2016
+//
+// Developer can be contacted by simon.m.mudd _at_ ed.ac.uk
+//
+//    Simon Mudd
+//    University of Edinburgh
+//    School of GeoSciences
+//    Drummond Street
+//    Edinburgh, EH8 9XP
+//    Scotland
+//    United Kingdom
+//
+// This program is free software;
+// you can redistribute it and/or modify it under the terms of the
+// GNU General Public License as published by the Free Software Foundation;
+// either version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY;
+// without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the
+// GNU General Public License along with this program;
+// if not, write to:
+// Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor,
+// Boston, MA 02110-1301
+// USA
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <sys/time.h>
+#include <fstream>
+#include "../LSDStatsTools.hpp"
+#include "../LSDChiNetwork.hpp"
+#include "../LSDRaster.hpp"
+#include "../LSDRasterInfo.hpp"
+#include "../LSDIndexRaster.hpp"
+#include "../LSDFlowInfo.hpp"
+#include "../LSDJunctionNetwork.hpp"
+#include "../LSDIndexChannelTree.hpp"
+#include "../LSDBasin.hpp"
+#include "../LSDChiTools.hpp"
+#include "../LSDParameterParser.hpp"
+#include "../LSDSpatialCSVReader.hpp"
+#include "../LSDShapeTools.hpp"
+
+int main (int nNumberofArgs,char *argv[])
+{
+
+  //Test for correct input arguments
+  if (nNumberofArgs!=3)
+  {
+    cout << "=========================================================" << endl;
+    cout << "|| Welcome to the chi mapping tool!                    ||" << endl;
+    cout << "|| This program has a number of options to make chi    ||" << endl;
+    cout << "|| plots and to map out slopes in chi space.           ||" << endl;
+    cout << "|| This program was developed by Simon M. Mudd         ||" << endl;
+    cout << "||  at the University of Edinburgh                     ||" << endl;
+    cout << "=========================================================" << endl;
+    cout << "This program requires two inputs: " << endl;
+    cout << "* First the path to the parameter file." << endl;
+    cout << "* Second the name of the param file (see below)." << endl;
+    cout << "---------------------------------------------------------" << endl;
+    cout << "Then the command line argument will be: " << endl;
+    cout << "In linux:" << endl;
+    cout << "./chi_mapping_tool.exe /LSDTopoTools/Topographic_projects/LSDTT_chi_examples/ Xian_example1.driver" << endl;
+    cout << "=========================================================" << endl;
+    cout << "For more documentation on the parameter file, " << endl;
+    cout << " see readme and online documentation." << endl;
+    cout << " https://lsdtopotools.github.io/LSDTopoTools_ChiMudd2014/" << endl;
+    cout << "=========================================================" << endl;
+    exit(EXIT_SUCCESS);
+  }
+
+  string path_name = argv[1];
+  string f_name = argv[2];
+
+  // load parameter parser object
+  LSDParameterParser LSDPP(path_name,f_name);
+
+  // for the chi tools we need georeferencing so make sure we are using bil format
+  LSDPP.force_bil_extension();
+
+  // maps for setting default parameters
+  map<string,int> int_default_map;
+  map<string,float> float_default_map;
+  map<string,bool> bool_default_map;
+  map<string,string> string_default_map;
+
+  // Basic DEM preprocessing
+  float_default_map["minimum_elevation"] = 0.0;
+  float_default_map["maximum_elevation"] = 30000;
+  float_default_map["min_slope_for_fill"] = 0.0001; // Set up a minimum slope when fillin' the dem to force a flow direction when nature,noise or flat areas create holes
+  bool_default_map["raster_is_filled"] = false; // assume base raster is already filled
+  bool_default_map["remove_seas"] = true; // elevations above minimum and maximum will be changed to nodata
+  string_default_map["CHeads_file"] = "NULL"; // channel head location extracted from another methods
+  bool_default_map["print_raster_without_seas"] = false;
+
+
+  // Selecting basins
+  int_default_map["threshold_contributing_pixels"] = 1000; // number of pixels in the basins
+  int_default_map["minimum_basin_size_pixels"] = 5000;
+  int_default_map["maximum_basin_size_pixels"] = 500000;
+  bool_default_map["test_drainage_boundaries"] = true;
+  bool_default_map["only_take_largest_basin"] = false;
+  string_default_map["BaselevelJunctions_file"] = "NULL";
+  bool_default_map["extend_channel_to_node_before_receiver_junction"] = true;
+
+  // IMPORTANT: S-A analysis and chi analysis wont work if you have a truncated
+  // basin. For this reason the default is to test for edge effects
+  bool_default_map["find_complete_basins_in_window"] = true;
+  bool_default_map["find_largest_complete_basins"] = false;
+  bool_default_map["print_basin_raster"] = false;
+
+  // printing of rasters and data before chi analysis
+  bool_default_map["convert_csv_to_geojson"] = false;  // This converts all cv files to geojson (for easier loading in a GIS)
+
+  bool_default_map["print_stream_order_raster"] = false;
+  bool_default_map["print_channels_to_csv"] = false;
+  bool_default_map["print_junction_index_raster"] = false;
+  bool_default_map["print_junctions_to_csv"] = false;
+  bool_default_map["print_fill_raster"] = false;
+  bool_default_map["print_DrainageArea_raster"] = false;
+  bool_default_map["write_hillshade"] = false;
+  bool_default_map["print_basic_M_chi_map_to_csv"] = false;
+
+  // This burns a raster value to any csv output of chi data
+  // Useful for appending geology data to chi profiles
+  bool_default_map["burn_raster_to_csv"] = false;
+  string_default_map["burn_raster_prefix"] = "NULL";
+  string_default_map["burn_data_csv_column_header"] = "burned_data";
+
+  // these print various basin and source data for visualisation
+  bool_default_map["print_source_keys"] = false;
+  bool_default_map["print_sources_to_csv"] = false;
+  bool_default_map["print_sources_to_raster"] = false;
+  bool_default_map["print_baselevel_keys"] = false;
+
+  // Use the parameter parser to get the maps of the parameters required for the
+  // analysis
+  LSDPP.parse_all_parameters(float_default_map, int_default_map, bool_default_map,string_default_map);
+  map<string,float> this_float_map = LSDPP.get_float_parameters();
+  map<string,int> this_int_map = LSDPP.get_int_parameters();
+  map<string,bool> this_bool_map = LSDPP.get_bool_parameters();
+  map<string,string> this_string_map = LSDPP.get_string_parameters();
+
+  // Now print the parameters for bug checking
+  cout << "PRINT THE PARAMETERS..." << endl;
+  LSDPP.print_parameters();
+
+  // location of the files
+  string DATA_DIR =  LSDPP.get_read_path();
+  string DEM_ID =  LSDPP.get_read_fname();
+  string OUT_DIR = LSDPP.get_write_path();
+  string OUT_ID = LSDPP.get_write_fname();
+  string raster_ext =  LSDPP.get_dem_read_extension();
+  vector<string> boundary_conditions = LSDPP.get_boundary_conditions();
+  string CHeads_file = LSDPP.get_CHeads_file();
+  string BaselevelJunctions_file = LSDPP.get_BaselevelJunctions_file();
+
+  //----------------------------------------------------------------------------//
+  // If you want, turn on all the appropriate switches for estimating the best
+  // fit m/n
+  //----------------------------------------------------------------------------//
+  if (this_bool_map["estimate_best_fit_movern"])
+  {
+    // we need to make sure we select basins the correct way
+    this_bool_map["find_complete_basins_in_window"] = true;
+    this_bool_map["print_basin_raster"] = true;
+    this_bool_map["write_hillshade"] = true;
+    this_bool_map["print_chi_data_maps"] = true;
+
+    // run the chi methods of estimating best fit m/n
+    this_bool_map["calculate_MLE_collinearity"] = true;
+    this_bool_map["calculate_MLE_collinearity_with_points_MC"] = true;
+    this_bool_map["print_profiles_fxn_movern_csv"] = true;
+    this_bool_map["movern_residuals_test"] = true;
+
+    // run the SA methods of estimating best fit m/n
+    this_bool_map["print_slope_area_data"] = true;
+    this_bool_map["segment_slope_area_data"] = true;
+  }
+
+  cout << "Read filename is: " <<  DATA_DIR+DEM_ID << endl;
+  cout << "Write filename is: " << OUT_DIR+OUT_ID << endl;
+
+  if(BaselevelJunctions_file == "NULL" || BaselevelJunctions_file == "Null" || BaselevelJunctions_file == "null" || BaselevelJunctions_file.empty() == true)
+  {
+    if(BaselevelJunctions_file.empty())
+    {
+      cout << "You have a null baselevel junctions file; the string is empty." << endl;
+    }
+    else
+    {
+      cout << "You have a null baselevel junctions file, it is: " << BaselevelJunctions_file << endl;
+    }
+  }
+  else
+  {
+    BaselevelJunctions_file = RemoveControlCharactersFromEndOfString(BaselevelJunctions_file);
+    BaselevelJunctions_file = DATA_DIR+BaselevelJunctions_file;
+    cout << "You have selected a baselevel junctions file, it is: " << BaselevelJunctions_file << endl;
+    cout << "Let me check if it exists..." << endl;
+
+    ifstream test_file;
+    test_file.open(BaselevelJunctions_file.c_str());
+    if( test_file.fail() )
+    {
+      cout << "\nWHOOPS the baselevel file: \"" << BaselevelJunctions_file
+         << "\" doesn't exist" << endl;
+      cout << "I am changing it to a NULL value!" << endl;
+      BaselevelJunctions_file = "NULL";
+    }
+    test_file.close();
+  }
+
+    // check to see if the raster exists
+  LSDRasterInfo RI((DATA_DIR+DEM_ID), raster_ext);
+
+  //============================================================================
+  // check to see if the raster for burning exists
+  LSDRaster BurnRaster;
+  bool burn_raster_exists = false;
+  string burn_raster_header = DATA_DIR+this_string_map["burn_raster_prefix"]+".hdr";
+  if (this_bool_map["burn_raster_to_csv"])
+  {
+    cout << "I am going to burn a raster to all your csv files. The header name for this raster is: " << endl;
+    cout <<  burn_raster_header << endl;
+  }
+  ifstream burn_head_in;
+  burn_head_in.open(burn_raster_header.c_str());
+  if( not burn_head_in.fail() )
+  {
+    burn_raster_exists = true;
+    string burn_fname = DATA_DIR+this_string_map["burn_raster_prefix"];
+    cout << "The burn raster exists. It has a prefix of: " << endl;
+    cout <<  burn_fname << endl;
+    LSDRaster TempRaster(burn_fname,raster_ext);
+    BurnRaster = TempRaster;
+  }
+  else
+  {
+    cout << "The burn raster doesn't exist! I am turning off the  burn flag" << endl;
+    this_bool_map["burn_raster_to_csv"] = false;
+  }
+  //============================================================================
+
+  // check the threshold pixels for chi
+  if (this_int_map["threshold_pixels_for_chi"] > this_int_map["threshold_contributing_pixels"])
+  {
+    cout << "WARNING: you have chosen a threshold pixels for chi which is greater" << endl;
+    cout << "   the threshold contributing pixels. Defaulting so these are equal." << endl;
+    this_int_map["threshold_pixels_for_chi"] = this_int_map["threshold_contributing_pixels"];
+  }
+
+  // initialise variables to be assigned from .driver file
+  // These will all be assigned default values
+  float A_0 = this_float_map["A_0"];
+  float movern = this_float_map["m_over_n"];
+  int n_iterations = this_int_map["n_iterations"];
+  int minimum_segment_length = this_int_map["minimum_segment_length"];
+  int maximum_segment_length = this_int_map["maximum_segment_length"];
+  int n_nodes_to_visit = this_int_map["n_nodes_to_visit"];             // when constructing channel network, this
+  float sigma = this_float_map["sigma"];
+  int target_nodes = this_int_map["target_nodes"];
+  int skip = this_int_map["skip"];
+  int threshold_contributing_pixels = this_int_map["threshold_contributing_pixels"];
+  int minimum_basin_size_pixels = this_int_map["minimum_basin_size_pixels"];
+  int basic_Mchi_regression_nodes = this_int_map["basic_Mchi_regression_nodes"];
+
+  // load the  DEM
+  LSDRaster topography_raster;
+  if (this_bool_map["remove_seas"])
+  {
+    cout << "I am removing high and low values to get rid of things that should be nodata." << endl;
+    LSDRaster start_raster((DATA_DIR+DEM_ID), raster_ext);
+    // now get rid of the low and high values
+    float lower_threshold = this_float_map["minimum_elevation"];
+    float upper_threshold = this_float_map["maximum_elevation"];
+    bool belowthresholdisnodata = true;
+    LSDRaster Flooded = start_raster.mask_to_nodata_using_threshold(lower_threshold,belowthresholdisnodata);
+    belowthresholdisnodata = false;
+    topography_raster = Flooded.mask_to_nodata_using_threshold(upper_threshold,belowthresholdisnodata);
+
+    if (this_bool_map["print_raster_without_seas"])
+    {
+      cout << "I'm replacing your raster with a raster without seas." << endl;
+      string this_raster_name = OUT_DIR+OUT_ID;
+      topography_raster.write_raster(this_raster_name,raster_ext);
+    }
+  }
+  else
+  {
+    LSDRaster start_raster((DATA_DIR+DEM_ID), raster_ext);
+    topography_raster = start_raster;
+  }
+  cout << "Got the dem: " <<  DATA_DIR+DEM_ID << endl;
