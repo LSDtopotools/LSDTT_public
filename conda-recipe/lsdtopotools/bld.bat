82effb27d6f672ceb706cc2ff1564ce6ded6213e
Stuart Grieve <s.grieve@qmul.ac.uk>
2018-09-06 10:53:53 +0100

    back to interactive none the wiser

--- conda-recipe/lsdtopotools/bld.bat
+++ conda-recipe/lsdtopotools/bld.bat
@@ -1,6 +1,6 @@
 set LDFLAGS="-Wl,-rpath,%PREFIX%\lib -L%PREFIX%\lib %LDFLAGS%"
 
-cmake -DCMAKE_INSTALL_PREFIX=%PREFIX% %SRC_DIR% -DCMAKE_INSTALL_BINDIR=bin
+cmake -DCMAKE_INSTALL_PREFIX=%PREFIX% %SRC_DIR% -DCMAKE_INSTALL_BINDIR=bin -G "Unix Makefiles"
 
 make
 make install
