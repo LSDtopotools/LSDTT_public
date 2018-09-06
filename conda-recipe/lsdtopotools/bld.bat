8578f5fd16d0627863e6e1b84d8d82c24023592c
Stuart Grieve <s.grieve@qmul.ac.uk>
2018-09-06 14:17:22 +0100

    trying to use the right version of msbuild

--- conda-recipe/lsdtopotools/bld.bat
+++ conda-recipe/lsdtopotools/bld.bat
@@ -2,4 +2,4 @@ cmake -DCMAKE_INSTALL_PREFIX=%PREFIX% %SRC_DIR% -DCMAKE_INSTALL_BINDIR=bin
 
 where msbuild
 
-msbuild %SRC_DIR%\ALL_BUILD.vcxproj
+C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe %SRC_DIR%\ALL_BUILD.vcxproj
