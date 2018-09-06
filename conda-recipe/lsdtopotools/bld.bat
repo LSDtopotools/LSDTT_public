1f5962ee79429473d9aa5d1d034023c80ac3827a
Stuart Grieve <s.grieve@qmul.ac.uk>
2018-09-06 13:00:56 +0100

    about as far as i can get fro now

--- conda-recipe/lsdtopotools/bld.bat
+++ conda-recipe/lsdtopotools/bld.bat
@@ -1,6 +1,3 @@
-set LDFLAGS="-Wl,-rpath,%PREFIX%\lib -L%PREFIX%\lib %LDFLAGS%"
+cmake -DCMAKE_INSTALL_PREFIX=%PREFIX% %SRC_DIR% -DCMAKE_INSTALL_BINDIR=bin
 
-cmake -DCMAKE_INSTALL_PREFIX=%PREFIX% %SRC_DIR% -DCMAKE_INSTALL_BINDIR=bin -G "Unix Makefiles"
-
-make
-make install
+MSBuild %SRC_DIR%\ALL_BUILD.vcxproj
