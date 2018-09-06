c4a45b2c55480edecc1587fc941f434646d999b4
Stuart Grieve <s.grieve@qmul.ac.uk>
2018-09-06 14:37:55 +0100

    we now have compiler errors

--- conda-recipe/lsdtopotools/bld.bat
+++ conda-recipe/lsdtopotools/bld.bat
@@ -1,5 +1,3 @@
 cmake -DCMAKE_INSTALL_PREFIX=%PREFIX% %SRC_DIR% -DCMAKE_INSTALL_BINDIR=bin
 
-where msbuild
-
-C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe %SRC_DIR%\ALL_BUILD.vcxproj
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" %SRC_DIR%\ALL_BUILD.vcxproj
