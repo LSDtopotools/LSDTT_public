cf39cd7923c0c66997e4da76bf311c3e21e9e3d0
Stuart Grieve <s.grieve@qmul.ac.uk>
2018-09-06 09:21:18 +0100

    changing forward slashes to backslashes in batch file

--- conda-recipe/lsdtopotools/bld.bat
+++ conda-recipe/lsdtopotools/bld.bat
@@ -1,5 +1,5 @@
 
-set LDFLAGS="-Wl,-rpath,%PREFIX%/lib -L%PREFIX%/lib %LDFLAGS%"
+set LDFLAGS="-Wl,-rpath,%PREFIX%\lib -L%PREFIX%\lib %LDFLAGS%"
 
 cmake -DCMAKE_INSTALL_PREFIX=%PREFIX% %SRC_DIR% -DCMAKE_INSTALL_BINDIR=bin
 
